65116e8995d662334473c7a176e76286b872c18d
wally <wally@within.co.kr>
2021-04-26 16:09:36 +0900

    6-8

---
+++ src/com/my/algorithm/chapter6/sort/Part8.java
@@ -0,0 +1,73 @@
+package com.my.algorithm.chapter6.sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ 8. 이분검색
+ 설명
+
+ 임의의 N개의 숫자가 입력으로 주어집니다. N개의 수를 오름차순으로 정렬한 다음 N개의 수 중 한 개의 수인 M이 주어지면
+
+ 이분검색으로 M이 정렬된 상태에서 몇 번째에 있는지 구하는 프로그램을 작성하세요. 단 중복값은 존재하지 않습니다.
+
+
+ 입력
+ 첫 줄에 한 줄에 자연수 N(3<=N<=1,000,000)과 M이 주어집니다.
+
+ 두 번째 줄에 N개의 수가 공백을 사이에 두고 주어집니다.
+
+
+ 출력
+ 첫 줄에 정렬 후 M의 값의 위치 번호를 출력한다.
+
+
+ 예시 입력
+ 8 32
+ 23 87 65 12 57 32 99 81
+
+ 예시 출력
+ 3
+ */
+public class Part8 {
+
+
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+
+        int[] ints = new int[n];
+
+        for(int i = 0; i < n; i++){
+            ints[i] = kb.nextInt();
+        }
+
+        Arrays.sort(ints);
+
+        int l = 0;
+        int r = ints.length-1;
+
+        int idx = -1;
+        while (l <= r){
+
+            int m = (l+r) / 2;
+
+            if(ints[m] == k){
+                idx = m;
+                break;
+            } else if(ints[m] > k){
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        System.out.print(idx + 1);
+        return;
+
+    }
+
+}
